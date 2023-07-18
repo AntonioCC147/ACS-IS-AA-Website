cb58a9a3e3fd55e1f8ff06d8cb1f3c0cb5593381
Crantea Antonio-Cristian <thet0mmy1407@gmail.com>
2023-07-18 17:55:55 +0300

    Finish setup

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,28 +1,23 @@
-// Link: https://reactrouter.com/en/main/components/link
-import { Link } from "react-router-dom";
-
-// Link: https://react-bootstrap.netlify.app/docs/layout/grid/#container
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-// Import CSS file
 import './home.css';
 
 export default function Home() {
     return (
         <Container fluid>
             <Row>
-                <Col sm={8} className="paragraph1">
-                    This is the first paragraph.
+                <Col sm={8}>
+                    1
                 </Col>
-                <Col sm={4} className="paragraph2">
-                    This is the second paragraph.
+                <Col sm={4}>
+                    2
                 </Col>
             </Row>
             <Row>
-                <Col className="link">
-                    Here is the <Link to="/demopage">link</Link> to a surprise.
+                <Col>
+                    3
                 </Col>
             </Row>
         </Container>
