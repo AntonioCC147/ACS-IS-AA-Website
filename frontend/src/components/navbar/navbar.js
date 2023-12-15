1340f79e26419b71ff4d03e9633b5d32ed2a6bb4
AntonioCC147 <77618117+AntonioCC147@users.noreply.github.com>
2023-12-15 12:50:26 +0200

    Solve navbar problem

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -12,12 +12,12 @@ function MyNavbar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto navbar-links">
-            <Nav.Link className="navbar-link1" href="/">Home</Nav.Link>
-            <Nav.Link className="navbar-link2" href="/materii">Insight Materii</Nav.Link>
-            <Nav.Link className="navbar-link3" target="blank" href="https://drive.google.com/drive/folders/1eMQ8SWBM3jM8GEOB7-n3FvW93oXZtg1Z?usp=sharing">Drive</Nav.Link>
+            <Nav.Link className="navbar-link1" href="/" style={{marginLeft: "100px"}}><div className="centrat">Home</div></Nav.Link>
+            <Nav.Link className="navbar-link2" href="/materii" style={{marginLeft: "100px"}}>Insight Materii</Nav.Link>
+            <Nav.Link className="navbar-link3" target="blank" href="https://drive.google.com/drive/folders/1eMQ8SWBM3jM8GEOB7-n3FvW93oXZtg1Z?usp=sharing" style={{marginLeft: "100px"}}>Drive</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link className="navbar-contact" href="/contact">Contact</Nav.Link>
+            <Nav.Link className="navbar-contact centerButton" href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
